45e6fa40a3d44e810edf2790fba791e4230dddd2
MikeyP957 <peraram2@gmail.com>
2022-04-14 22:54:35 -0700

    initial files and packages

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">Welcome to the Climber's Compass</div>;
+}
+
+export default App;
