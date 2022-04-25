a5f3192adcc49ec5418f41aad34df0cc2df1fdab
MikeyP957 <peraram2@gmail.com>
2022-04-25 13:24:06 -0700

    update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,26 @@
 import { Express, Request, Response } from "express";
+
+// controllers
+import { createUserSessionHandler } from "../controller/session.controller";
 import { createUserHandler } from "../controller/user.controller";
-import validateRequest from '../middleware/validateRequest'
+
+// middleware
+import validateResouce from "../middleware/validateResource";
+
+// schemas
 import { createUserSchema } from "../schema/user.schema";
+import { createSessionSchema } from "../schema/session.schema";
 
 export default function (app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
-  app.post("/api/users", validateRequest(createUserSchema), createUserHandler)
+  // Register User
+  app.post("/api/users", validateResouce(createUserSchema), createUserHandler);
+
+  // Login
+  app.post(
+    "/api/sessions",
+    validateResouce(createSessionSchema),
+    createUserSessionHandler
+  );
 }
