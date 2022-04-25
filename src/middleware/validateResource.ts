e36dc3a9dacf49a4524ca2ec01237cc45fcc7ebe
MikeyP957 <peraram2@gmail.com>
2022-04-25 13:23:35 -0700

    refactor with zod npm package

--- src/middleware/validateResource.ts
+++ src/middleware/validateResource.ts
@@ -1,21 +1,21 @@
-import type { AnySchema } from "yup";
+import { AnyZodObject } from "zod";
 import { Request, Response, NextFunction } from "express";
 import log from "../logger";
 
 const validate =
-  (schema: AnySchema) =>
+  (schema: AnyZodObject) =>
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      await schema.validate({
+      await schema.parse({
         body: req.body,
         query: req.query,
         params: req.params,
       });
-      return next()
+      return next();
     } catch (error: any) {
       log.error(error);
       return res.status(400).send(error.error);
     }
   };
 
-  export default validate 
+export default validate;
