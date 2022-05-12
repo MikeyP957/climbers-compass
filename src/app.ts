7baf837e6657ac85d7ce4de87dc9be72567c3a3d
MikeyP957 <peraram2@gmail.com>
2022-05-12 15:04:23 -0700

    create server function created for testing

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,16 @@
-import express from "express";
 import config from "../config/default";
 import log from "./utils/logger";
 import connect from "./utils/connect";
-import routes from "./routes";
 
-// middleware
-import deserializeUser from "./middleware/deserializeUser";
+import createServer from "./utils/server";
 
 const port = config.port as number;
 const host = config.host as string;
 
-const app = express();
-
-app
-  .use(express.json())
-  .use(express.urlencoded({ extended: false }))
-  .use(deserializeUser);
+const app = createServer();
 
 app.listen(port, host, () => {
   log.info(`server listening at http://${host}:${port}`);
 
   connect();
-
-  routes(app);
 });
