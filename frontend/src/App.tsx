451be06cf4fae6681dac796c11de8e33a266ec4f
MikeyP957 <peraram2@gmail.com>
2022-05-17 16:44:53 -0400

    remove frontend to change to next.js

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return <div className="App">Welcome to the Climber's Compass</div>;
-}
-
-export default App;
