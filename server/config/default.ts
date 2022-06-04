7f2989b04c2fb630e707fb2caf4d91b4c4ddf99c
MikeyP957 <peraram2@gmail.com>
2022-06-04 14:24:02 -0700

    add cors headers

--- server/config/default.ts
+++ server/config/default.ts
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 export default {
   port: 1337,
+  origin: "http://localhost:3000",
   host: "localhost",
   dbUri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fxjmm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
   saltWorkFactor: 10,
--- server/src/utils/server.ts
+++ server/src/utils/server.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import cors from "cors";
 import deserializeUser from "../middleware/deserializeUser";
 import routes from "../routes";
+import config from "../../config/default";
 
 function createServer() {
   const app = express();
 
   app
+    .use(
+      cors({
+        origin: config.origin,
+        credentials: true,
+      })
+    )
     .use(express.json())
     .use(express.urlencoded({ extended: false }))
     .use(deserializeUser);
